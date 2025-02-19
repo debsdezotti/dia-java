a03507f0b031aa595c44c578de3e21dd5bba30a7
debsdezotti <theb.contact@gmail.com>
2025-02-19 19:42:30 -0300

    otimizacao saque

--- Aula-Java_Dois/Sistema-Conta/src/Maquina.java
+++ Aula-Java_Dois/Sistema-Conta/src/Maquina.java
@@ -38,9 +38,7 @@ public class Maquina {
         System.out.println("Seu Cheque Especial atual é de: " + chequeespecial);
     }
 
-    public void sacarDinheiro() {
-        System.out.println("Qual o valor do saque? ");
-        this.saque = scanner.nextDouble();
+    public void realizarSaque(double saque, String descricao) {
         System.out.println("Sacando R$" + saque + "...");
     
         // Verifica se o saldo é suficiente
@@ -48,9 +46,9 @@ public class Maquina {
             System.out.println("Saldo insuficiente. (Saldo atual: R$" + saldo + ")");
         }
         // Verifica se o saque pode ser realizado sem usar o cheque especial
-        else if ((saldo - chequeespecial) > saque && saldo > saque) {
+        else if ((saldo - chequeespecial) > saque && saldo >= saque) {
             saldo -= saque; // Atualiza o saldo
-            System.out.println("Realizando saque...");
+            System.out.println("Realizando " + descricao + "...");
             System.out.println("Saldo atualizado para R$" + saldo);
         }
         // Verifica se será necessário usar o cheque especial
@@ -58,10 +56,22 @@ public class Maquina {
             taxa = (saque - (saldo - chequeespecial)) * 0.2;
             saldo -= saque; // Atualiza o saldo
             usochequeespecial = true; // Marca que o cheque especial foi usado
-            System.out.println("Realizando saque...");
+            System.out.println("Realizando " + descricao + "...");
             System.out.println("Saldo atualizado para R$" + saldo);
             System.out.println("Taxa do cheque especial R$" + taxa);
         }
+    }
+
+    public void sacarDinheiro() {
+        System.out.println("Qual o valor do saque? ");
+        this.saque = scanner.nextDouble();
+        realizarSaque(saque, "Saque");
+    }
+
+    public void pagarBoleto() {
+        System.out.println("Qual o valor do boleto? ");
+        this.saque = scanner.nextDouble();
+        realizarSaque(saque, "pagamento");
     }
     
 
@@ -78,30 +88,6 @@ public class Maquina {
         }
     }
 
-    public void pagarBoleto() {
-        System.out.println("Qual o valor do boleto? ");
-        this.saque = scanner.nextDouble();
-        System.out.println("Sacando R$" + saque + "...");
-        // não realizará o saque
-        if (saldo < saque) {
-            System.out.println("Saldo insuficiente. (Saldo atual: R$" + saldo + ")");
-        }
-        // não usará limite
-        if ((saldo - chequeespecial) > saque) {
-            saldo = this.saldo - saque;
-            System.out.println("Pagando Boleto...");
-            System.out.println("Saldo atualizado para R$" + saldo);
-        }
-        // usará limite
-        if ((saldo - chequeespecial) < saque && saldo > saque ) {
-            taxa = (saque - (saldo - chequeespecial))*0.2;
-            saldo = this.saldo - saque;
-            System.out.println("Pagando Boleto...");
-            System.out.println("Saldo atualizado para R$" + saldo);
-            System.out.println("Taxa do cheque especial de R$" + taxa);
-            this.usochequeespecial = true;
-        }
-        
-    }
+
 
 }
