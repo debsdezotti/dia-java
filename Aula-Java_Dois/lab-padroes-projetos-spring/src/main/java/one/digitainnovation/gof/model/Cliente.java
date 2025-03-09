7b84c7f883c600be1b034e9ed20fe0681e82230f
debsdezotti <theb.contact@gmail.com>
2025-03-09 15:17:02 -0300

    teste

--- Aula-Java_Dois/lab-padroes-projetos-spring/src/main/java/one/digitainnovation/gof/model/Cliente.java
+++ Aula-Java_Dois/lab-padroes-projetos-spring/src/main/java/one/digitainnovation/gof/model/Cliente.java
@@ -1,10 +1,7 @@
 package one.digitalinnovation.gof.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Cliente {
